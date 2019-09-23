20d3a24585a4919ae54e794868b82c262b1e8048
Anirban Chakraborty <chk.anirban@gmail.com>
2019-09-23 18:22:51 +0530

    formatted

--- src/main/java/me/anichakra/poc/simple/rest/controller/AssetDataController.java
+++ src/main/java/me/anichakra/poc/simple/rest/controller/AssetDataController.java
@@ -22,9 +22,6 @@ public class AssetDataController {
     @Autowired
     private DataService dataService;
 
-    
-    
-    
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{account}")
     @ResponseBody
