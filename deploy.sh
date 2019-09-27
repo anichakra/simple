0d659b38a86a7eae1db89d456a415bbc1432c5d6
Anirban Chakraborty <chk.anirban@gmail.com>
2019-09-27 22:11:32 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-aws ecs update-service --cluster cloudnativelab-ecs-cluster --service simple-rest-service --force-new-deployment --region us-east-1
+#aws ecs update-service --cluster cloudnativelab-ecs-cluster --service simple-rest-service --force-new-deployment --region us-east-1
+
+aws ecr get-login --region us-east-1 | xargs xargs
