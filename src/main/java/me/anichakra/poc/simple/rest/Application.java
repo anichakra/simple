8dbfd38cd3585915a74896c8c65c5f290c5edc80
Anirban Chakraborty <chk.anirban@gmail.com>
2019-09-29 18:41:44 +0530

    update

--- src/main/java/me/anichakra/poc/simple/rest/Application.java
+++ src/main/java/me/anichakra/poc/simple/rest/Application.java
@@ -2,7 +2,6 @@ package me.anichakra.poc.simple.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
 
 @SpringBootApplication
 //@EnableContextInstanceData
