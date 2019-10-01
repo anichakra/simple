a99b1b9f9ceb76ac500f4998b2191a64a49bb20a
Anirban Chakraborty <chk.anirban@gmail.com>
2019-10-01 14:49:35 +0530

    updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -55,7 +55,15 @@ node {
           sh("mvn sonar:sonar -Dsonar.projectKey=" + ARTIFACT_ID + " -Dsonar.host.url=" + SONAR_URL + " -Dsonar.login=" + SONAR_TOKEN)
         }
       }
-  
+      
+ //   stage("Sonar Quality Gate"){
+   //   timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+  //      def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+  //      if (qg.status != 'OK') {
+  //        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+  //      }
+  //    }
+   // }
     
      stage('JAR Install') {
         println "########## Installing jar files in local maven repository ##########"
--- src/main/java/me/anichakra/poc/simple/rest/Application.java
+++ src/main/java/me/anichakra/poc/simple/rest/Application.java
@@ -1,5 +1,9 @@
 package me.anichakra.poc.simple.rest;
 
+import java.nio.file.SecureDirectoryStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         System.setProperty(
-                "node.id", String.valueOf(Math.random()).substring(2, 4));
+                "node.id", String.valueOf(SecureRandom.getInstanceStrong().nextLong()).substring(2, 4));
         SpringApplication.run(Application.class, args);
     }
-
 }
