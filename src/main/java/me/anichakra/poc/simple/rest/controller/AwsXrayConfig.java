fc77b8947a5a8c718223a1d1d2512c9f19d68803
Anirban Chakraborty <chk.anirban@gmail.com>
2020-01-09 22:37:14 +0530

    add cors

--- src/main/java/me/anichakra/poc/simple/rest/controller/AwsXrayConfig.java
+++ src/main/java/me/anichakra/poc/simple/rest/controller/AwsXrayConfig.java
@@ -18,7 +18,7 @@ public class AwsXrayConfig {
 	public Filter tracingFilter() {
 		return new AWSXRayServletFilter("simple-rest-service") {
 			public Segment preFilter(ServletRequest request, ServletResponse response) {
-				((HttpServletResponse) response).addHeader("Abc", "Anirban");
+				((HttpServletResponse) response).addHeader("X-Amzn-My-Name", "Anirban");
 
 				return super.preFilter(request, response);
 
